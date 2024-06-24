c76f0e869602b2840f4b6abbcfa44437e228f785
Anand_Singh <randareak@gmail.com>
2024-06-24 05:44:41 +0530

    2LecDone

---
+++ 01_variables/variable.js
@@ -0,0 +1,6 @@
+// There are three types of variable :
+
+// var:can be re-declared or updated. Global Scope variable(SHOULD NEVER USE)
+// let: Can't be re-declared but can be updated. Block Scope variable 
+// const:Can neither be re-declared nor updated. Block Scope variable
+
---
+++ 02_dataTyps_ECMA/dataTypes_ECMA.js
@@ -0,0 +1,27 @@
+// "use strict"; //treat all js code as newer version
+
+// alert(3+3); //we are using node.js not browser
+
+console.log(3+3); //code readability is most important
+console.log("is the answer");
+
+let name = "Anand Singh";
+let age = 19;
+let goodDev = true;
+let pastExperience = null;
+let state;
+
+//                      PRIMITIVE DATATYPES
+
+// number => 2^53 (limit)
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => no value/datatype asigned until now
+// symbol => unique
+
+// object
+
+console.log(typeof pastExperience); // output= object
+console.log(typeof state); // output= undefined
